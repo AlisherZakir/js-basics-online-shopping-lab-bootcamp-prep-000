1c7016c84aaa64fba39861369f07d5cdf1e8ea08
Alisher Zakir <a3zakir@gmail.com>
2019-04-19 12:47:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,18 +11,43 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var price = Math.ceil(Math.random()* 100);
+ cart.push({itemName: item, itemPrice: price});
 }
 
 function viewCart() {
   // write your code here
+  if (cart.length < 1) {
+    return "Your shopping cart is empty."
+  } else {
+    var text = "In your cart, you have "
+    for (var i = 0; i < cart.length; i++) {
+      var name = cart[i].itemName;
+      var price = cart[i].itemPrice
+      if (i!==0) {
+        text += `,`
+      }
+      if (i + 1 === cart.length) {
+        
+      }
+      text += `${name} at $${price}`
+    }
+    return text + "."
+  }
 }
 
 function total() {
   // write your code here
+  return cart.reduce((s, {itemPrice}) => s + itemPrice, 0);
 }
 
 function removeFromCart(item) {
   // write your code here
+  if (cart.findIndex(({itemName}) => itemName === item) === -1) {
+    return "That item is not in your cart."
+  } else {
+    cart = cart.filter(({itemName}) => itemName !== item);
+  }
 }
 
 function placeOrder(cardNumber) {
